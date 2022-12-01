360b13fb93be6bc41fff8878856743734a41690b
Viktor Sandström <sandstrom.viktor@gmail.com>
2022-12-01 13:15:20 +0100

    gitignore

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/viktorsandstrom/Documents/Rust/adventofcode2022/day1/target/debug/day1: /Users/viktorsandstrom/Documents/Rust/adventofcode2022/day1/src/main.rs
--- day1/target/debug/deps/day1-6b87ef8afb6af0f1.d
+++
@@ -1,5 +0,0 @@
-/Users/viktorsandstrom/Documents/Rust/adventofcode2022/day1/target/debug/deps/day1-6b87ef8afb6af0f1.rmeta: src/main.rs
-
-/Users/viktorsandstrom/Documents/Rust/adventofcode2022/day1/target/debug/deps/day1-6b87ef8afb6af0f1.d: src/main.rs
-
-src/main.rs:
