71d7c84baa79d9639cae23a9859a0cc18d1e653c
Viktor Sandström <sandstrom.viktor@gmail.com>
2022-12-24 00:40:28 +0100

    day8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashSet};
 
 fn parse(input: &str) -> Result<String, std::io::Error> {
     return std::fs::read_to_string(input)
@@ -14,8 +14,75 @@ fn parse_forest(input: &str) -> Vec<Vec<i8>>{
     trees
 }
 
-fn calc(coord: (i8, i8), forest: &Vec<Vec<i8>>) -> [u32; 4] {
-    [0,0,0,0]
+fn parse_forest32(input: &str) -> Vec<Vec<i32>>{
+    let mut trees: Vec<Vec<i32>> = vec![];
+    for line in input.lines() {
+        // let lat: Vec<u32> = line.chars().collect::<Vec<char>>().into_iter().map(|x| x.to_string().parse::<u32>().unwrap()).collect::<Vec<u32>>();
+        let lat = line.chars().collect::<Vec<char>>().into_iter().map(|x| x as i32 - '0' as i32).collect::<Vec<i32>>();
+        trees.push(lat);
+    }
+    trees
+}
+
+fn calc(coord: (usize, usize), forest: &Vec<Vec<i32>>) -> i32 {
+    let mut dirs: [i32; 4] = [0,0,0,0];
+    let c = coord;
+
+    // outer loop variable translation
+    // coord.0 == i
+    // coord.1 == j
+
+    for j in (0..c.1).rev() {
+        // upp
+        if forest[c.0][c.1] <= forest[c.0][j] {
+            dirs[0] += 1;
+            break;
+        } else {
+            dirs[0]+=1;
+        }
+    }
+    
+    for i in (0..c.0).rev() {
+        // väst
+        if forest[c.0][c.1] <= forest[i][c.1] {
+            dirs[1] += 1;
+            break;
+        } else {
+            dirs[1]+=1;
+        }
+    }
+
+    for i in c.0+1..forest.len() {
+        // öst 
+        if forest[c.0][c.1] <= forest[i][c.1] {
+            dirs[2] += 1;
+            break;
+        } else {
+            dirs[2]+=1;
+        }
+    }
+    
+    for j in c.1+1..forest[0].len() {
+        // ner 
+
+        if forest[c.0][c.1] <= forest[c.0][j] {
+            dirs[3] += 1;
+            break;
+        } else {
+            dirs[3]+=1;
+        }
+    }
+
+    // let mut sum = 1;
+    // for num in dirs {
+    //     sum = sum * num;
+    //
+    // }
+    // sum
+    //
+
+    dirs.iter().fold(1, |acc, &x|{ acc*x})
+
 }
 
 fn task1(trees: &Vec<Vec<i8>>) -> HashSet<(i8, i8)>{
@@ -66,124 +133,29 @@ fn task1(trees: &Vec<Vec<i8>>) -> HashSet<(i8, i8)>{
 }
 
 
-fn task2(trees: &Vec<Vec<i8>>) -> HashSet<(i8, i8)>{
-    let mut visible: HashSet<(i8, i8)> = HashSet::new();
-    // -1 for the last row on each run
-
-    for i in 0..trees.len() {
-        let mut ltall = -1;
-        for j in 0..trees[i].len() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-            }
-        } 
-    }
-    
-    for i in 0..trees.len() {
-        let mut ltall = -1;
-        for j in (0..trees[i].len()).rev() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-            }
-        } 
-    }
-    
-    for j in 0..trees[0].len() {
-        let mut ltall = -1;
-        for i in 0..trees.len() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-        }
-        } 
-    }
-    
-    for j in 0..trees[0].len() {
-        let mut ltall = -1;
-        for i in (0..trees.len()).rev() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-            }
-        } 
-    }
-
-    visible
-}
-
-
-fn task2(trees: &Vec<Vec<i8>>) -> HashSet<(i8, i8)>{
-    let mut visible: HashSet<(i8, i8)> = HashSet::new();
-    // -1 for the last row on each run
-
-    for i in 0..trees.len() {
-        let mut ltall = -1;
-        for j in 0..trees[i].len() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-            }
-        } 
-    }
-    
-    for i in 0..trees.len() {
-        let mut ltall = -1;
-        for j in (0..trees[i].len()).rev() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-            }
-        } 
-    }
-    
-    for j in 0..trees[0].len() {
-        let mut ltall = -1;
-        for i in 0..trees.len() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-        }
-        } 
-    }
-    
-    for j in 0..trees[0].len() {
-        let mut ltall = -1;
-        for i in (0..trees.len()).rev() {
-            if trees[i][j] > ltall {
-                visible.insert((i as i8, j as i8));
-                ltall = trees[i][j];
-            }
-        } 
-    }
-
-    visible
-}
-
-fn task2(forest: &Vec<Vec<i8>>) {
+fn task2(forest: &mut Vec<Vec<i32>>) -> i32 {
     // räkna hur många träd bort man ser, inklusive det träd som blockar vyn
     // nord * väst * syd * öst
-    
-    for i in 0..forest.len() {
-        // north, south, west, east
-        let mut dirs: [u32; 4] = [0,0,0,0];
-        for j in 0..forest[i].len() {
-            dirs = calc((i as i8, j as i8), &forest);
-
-
-        }
-
-
+    let mut highest = 0;
+    for i in 1..forest.len()-1 {
+        for j in 1..forest[i].len()-1 {
+            let temp = calc((i,j), forest);
+            if temp > highest {
+                highest = temp;
+            }
+        } 
     }
-
-    todo!()
+    highest
 }
 
 fn main() {
-    let forest = parse_forest(&parse("input.txt").unwrap());
+    let input = parse("input.txt").unwrap();
+    let forest = parse_forest(&input);
+
+    let mut forest2: Vec<Vec<i32>> = parse_forest32(&input);
     let visible = task1(&forest);
     println!("{}", visible.len());
+    println!("{}", task2(&mut forest2));
 
 }
 
@@ -192,7 +164,6 @@ mod tests {
     use super::*;
 
     const TEST1: &str = "test.txt";
-    const TEST2: &str = "test2.txt";
 
     #[test]
     fn test1_result(){
@@ -245,14 +216,13 @@ mod tests {
 
     #[test]
     fn test_case2_1() {
-        let forest = parse_forest(&parse(TEST2).unwrap());
-        assert_eq!(4, task2(&forest));
+        let mut forest = parse_forest32(&parse(TEST1).unwrap());
+        assert_eq!(4, calc((1, 2), &mut forest));
     }
-
+    
     #[test]
-    fn test_case2_1() {
-        let forest = parse_forest(&parse(TEST2).unwrap());
-        assert_eq!(4, task2(&forest));
+    fn test_case2_2() {
+        let mut forest = parse_forest32(&parse(TEST1).unwrap());
+        assert_eq!(8, calc((3,2), &mut forest));
     }
-
 }
